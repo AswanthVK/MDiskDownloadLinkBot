5a04bd1774ba5de3f343cdb8b1639a5ff6db5869
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-08-26 13:17:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from pyrogram import Client, filters
 
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-API_ID = int(os.environ.get("API_ID"))
+API_ID = int(os.environ.get("API_ID", 1234))
 
 API_HASH = os.environ.get("API_HASH", "")
 
