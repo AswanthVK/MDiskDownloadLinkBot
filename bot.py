83a5eb109eda5c29fb6aa35170c527c862815515
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-10-21 23:52:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import math
 import time
 from datetime import timedelta
 from pyrogram import Client, filters
+from helper_funcs.helpers import humanbytes, convert
 
 
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
@@ -16,24 +17,6 @@ API_HASH = os.environ.get("API_HASH", "")
 
 
 app = Client("tgid", bot_token=TG_BOT_TOKEN, api_hash=API_HASH, api_id=APP_ID)
-
-
-def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
-    if not size:
-        return ""
-    power = 2**10
-    n = 0
-    Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
-
-def convert(n):
-   return str(timedelta(seconds = n))
 
 
 @app.on_message(filters.command(['start']))
